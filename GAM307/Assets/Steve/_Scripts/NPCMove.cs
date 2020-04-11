42a3f0bf54ba99682704417aecf6994adea0d04d
Steven <spkranjac@gmail.com>
2020-04-11 14:34:32 +1000

    NavMesh Begining
    - Simple nav mesh created to one point

---
+++ GAM307/Assets/Steve/_Scripts/NPCMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class NPCMove : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        if(_navMeshAgent == null)
+        {
+            Debug.LogError(" No nav mesh component attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+        
+    }
+
+    // Destination 
+    void SetDestination()
+    {
+        if(_destination != null)
+        {
+            Vector3 targetVector = _destination.transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+        }
+    }
+}
