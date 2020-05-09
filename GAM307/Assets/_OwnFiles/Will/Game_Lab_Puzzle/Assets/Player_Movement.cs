b2f9d84a3d20a3fadf7747fd43a42cb3f4552b6b
Will <wchidiak@myjmc.edu.au>
2020-05-09 16:33:28 +1000

    Switch Puzzle

---
+++ GAM307/Assets/_OwnFiles/Will/Game_Lab_Puzzle/Assets/Player_Movement.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    private float sensitivity = 150f;
+
+    public Camera playerCam;
+
+    private float xRotation;
+
+    Rigidbody rb;
+    CapsuleCollider col;
+    float colY;
+
+    public float playerSpeed = 5f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<CapsuleCollider>();
+        colY = col.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(value: xRotation, min: -90, max: 90);
+
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, y: 0, z: 0);
+        transform.Rotate(Vector3.up * mouseX);
+
+        /*if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            StartCrouch();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            StopCrouch();
+        }*/
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            //rb.AddRelativeForce(new Vector3(h, rb.velocity.y, v) * playerSpeed, ForceMode.VelocityChange);
+
+            rb.velocity = transform.rotation * new Vector3(h, 0, v) * playerSpeed + new Vector3(0, rb.velocity.y, 0);
+    }
+
+    /*private void StartCrouch()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        col.height = colY * .2F;
+    }
+
+    private void StopCrouch()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        col.height = colY;
+        transform.localScale = new Vector3(x: 1, y: 1, z: 1);
+    }*/
+}
---
+++ GAM307/Assets/_OwnFiles/Will/Game_Lab_Puzzle/Assets/Switch.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject Interact_Prompt;
+
+    public bool IsSwitchFlipped = false;
+    public bool IsDoorActivated = false;
+
+    public Animator SwitchFlip;
+    public Animator DoorOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      SwitchFlip = GetComponent<Animator>();
+      DoorOpen = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Interact_Prompt.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Interact_Prompt.SetActive(false);
+                SwitchOn();
+            }
+        }
+    }
+
+    public void SwitchOn()
+    {
+        if(IsSwitchFlipped == false)
+        {
+            IsSwitchFlipped = true;
+            SwitchFlip.SetBool("IsSwitchFlipped", true);
+        }
+        else
+        {
+            IsSwitchFlipped = false;
+            SwitchFlip.SetBool("IsSwitchFlipped", false);
+        }
+        Debug.Log("Switch");
+    }
+
+    public void DoorActivated()
+    {
+        if (IsDoorActivated == false)
+        {
+            IsDoorActivated = true;
+            DoorOpen.SetBool("IsDoorActivated", true);
+        }
+        else
+        {
+            IsDoorActivated = false;
+            DoorOpen.SetBool("IsDoorActivated", false);
+        }
+        Debug.Log("Door");
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Interact_Prompt.SetActive(false);
+        }
+    }
+}
